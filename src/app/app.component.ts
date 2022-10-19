333d776ef4edd576fa1267caeb37bff47b68920c
abdelati <chraiha1abdelati@gmail.com>
2022-10-19 23:44:25 +0100

    delete posh notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { OneSignal } from 'onesignal-ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculatorFitness';
-
-  constructor(private oneSignal: OneSignal) {
-    this.oneSignal.init({
-      appId: "da636ddc-dfa3-45d5-8263-4cb7a9fbf361",
-    });
-  }
+  title:string = 'calculatorFitness';
 }
