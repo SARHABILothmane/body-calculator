5e03d27e8a6139f0d069fdfffc789ba980c866a4
abdelati <chraiha1abdelati@gmail.com>
2022-10-26 22:48:04 +0100

    add content random number generateur

--- src/app/pages/math/math-routing.module.ts
+++ src/app/pages/math/math-routing.module.ts
@@ -1,6 +1,3 @@
-import { HexCalculatorModule } from './hex-calculator/hex-calculator.module';
-import { PercentageErrorCalculatorModule } from './percentage-error-calculator/percentage-error-calculator.module';
-import { PercentageCalculatorModule } from './percentage-calculator/percentage-calculator.module';
 import { MathComponent } from './math.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -8,11 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: "", component: MathComponent, children: [
-      { path: "random-number-generator", loadChildren: () => import("src/app/pages/math/random-number/random-number.module").then(mod => mod.RandomNumberModule) },
-      { path: "percentage-calculator", loadChildren: () => import("src/app/pages/math/percentage-calculator/percentage-calculator.module").then(mod => mod.PercentageCalculatorModule) },
-      { path: "percentage-error-calculator", loadChildren: () => import("src/app/pages/math/percentage-error-calculator/percentage-error-calculator.module").then(mod => mod.PercentageErrorCalculatorModule) },
-      { path: "binary-calculator", loadChildren: () => import("src/app/pages/math/binary-calculator/binary-calculator.module").then(mod => mod.BinaryCalculatorModule) },
-      { path: "hex-calculator", loadChildren: () => import("src/app/pages/math/hex-calculator/hex-calculator.module").then(mod => mod.HexCalculatorModule) },
+      { path: "random-number-generator/.", loadChildren: () => import("src/app/pages/math/random-number/random-number.module").then(mod => mod.RandomNumberModule) },
+      { path: "percentage-calculator/.", loadChildren: () => import("src/app/pages/math/percentage-calculator/percentage-calculator.module").then(mod => mod.PercentageCalculatorModule) },
+      { path: "percentage-error-calculator/.", loadChildren: () => import("src/app/pages/math/percentage-error-calculator/percentage-error-calculator.module").then(mod => mod.PercentageErrorCalculatorModule) },
+      { path: "binary-calculator/.", loadChildren: () => import("src/app/pages/math/binary-calculator/binary-calculator.module").then(mod => mod.BinaryCalculatorModule) },
+      { path: "hex-calculator/.", loadChildren: () => import("src/app/pages/math/hex-calculator/hex-calculator.module").then(mod => mod.HexCalculatorModule) },
 
     ]
   }
